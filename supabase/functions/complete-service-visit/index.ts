c335006c5b69a73734f9bc829b0bcdcfb24876cf
SdZika <markozivkovic1982@gmail.com>
2026-08-12 23:22:04 +0200

    add deno.json

--- supabase/functions/complete-service-visit/index.ts
+++ supabase/functions/complete-service-visit/index.ts
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
 
